0b6bc3ca8ecf3fe100a3f4d3e316d49917b6a253
christine <credhead89@gmail.com>
2020-02-28 16:11:23 -0500

    created backend restful routes for meetings controller

--- app/controllers/api/meetings_controller.rb
+++ app/controllers/api/meetings_controller.rb
@@ -1,2 +1,54 @@
 class Api::MeetingsController < ApplicationController
+  def index
+    @meetings = Meeting.all
+    render "index.json.jb"
+  end
+
+  def show
+    @meeting = Meeting.find(params[:id])
+    render 'show.json.jb'
+  end
+
+  def create
+    @meeting = Meeting.new(
+      seeker_id: params[:seeker_id],
+      fulfiller_id: params[:fulfiller_id],
+      location: params[:location],
+      start_time: params[:start_time],
+      end_time: params[:end_time]  
+    )
+    if @meeting.save
+      # happy path
+      render 'show.json.jb'
+    else
+      # sad path
+      render json: {errors: @meeting.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @meeting = Meeting.find(params[:id])
+
+    @meeting.location = params[:location] || @meeting.location
+    @meeting.start_time = params[:start_time] || @meeting.start_time
+    @meeting.end_time = params[:end_time] || @meeting.end_time
+
+    if @meeting.save
+      # happy path
+      render 'show.json.jb'
+    else
+      # sad path
+      render json: {errors: @meeting.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @meeting = Meeting.find(params[:id])
+    @user.destroy
+    render json: {message: "User successfully destroyed"}
+    
+  end
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,19 @@ Rails.application.routes.draw do
     patch "/users/:id" => "users#update"
     delete "/users/:id" => "users#destroy"
 
+    get "/meetings" => "meetings#index"
+    get "/meetings/:id" => "meetings#show"
+    post "/meetings" => "meetings#create"
+    patch "/meetings/:id" => "meetings#update"
+    delete "/meetings/:id" => "meetings#destroy"
+
+    get "/reviews" => "reviews#index"
+    get "/reviews/:id" => "reviews#show"
+    post "/reviews" => "reviews#create"
+    patch "/reviews/:id" => "reviews#update"
+    delete "/reviews/:id" => "reviews#destroy"
+
+
+
   end
 end
