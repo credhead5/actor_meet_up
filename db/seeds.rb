211277913f8e1845959587df9caa38822590afad
christine <credhead89@gmail.com>
2020-03-25 18:28:42 -0400

    updated user logic in meeting show view

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,11 +21,12 @@
 User.create!([
   {username: 'bbmax', email: 'bbmax@gmail.com', bio: "Actor from New York", password: "password", image_url: "https://i.pinimg.com/originals/51/79/7f/51797f1fe0b98932291b87527c115470.jpg"},
   {username: 'cad879', email: 'cad879@gmail.com', bio: "Actor from Ohio", password: "password", image_url: "https://i.pinimg.com/originals/29/80/94/298094c2d1dbf6267d5e61d741b902c2.jpg"},
-  {username: 'goddsam13', email: 'godd13@email.com', bio: "Actor from LA", password: "password", image_url: "https://i.pinimg.com/236x/97/3c/e9/973ce9f32f4038db7c42b32d02db8360--headshot-poses-actor-headshots.jpg"},
-  {username: 'swaddle56', email: 'swaddle56.com', bio: "American actor, writer, producer, comedian, and political activist. ", password: "password", image_url: "https://www.smartheadshots.com/actor-headshots/headshots/headshots-5225.jpg"},
-  {username: 'twittle12', email: 'twittle12', bio: "Australian actor, singer, and producer", password: "password", image_url: "https://static1.squarespace.com/static/5aa5d49e297114f4397667c5/t/5e41c208342d6509539ca3c5/1581367816893/Sam+Wright+%28500px%29.jpg"},
+  {username: 'goddsam13', email: 'godd13@email.com', bio: "Actor from LA", password: "password", image_url: "https://i.pinimg.com/474x/22/99/fb/2299fb7caf07e0df716fb7c0db322f43.jpg"},
+  {username: 'swaddle56', email: 'swaddle56@email.com', bio: "American actor, writer, producer, comedian, and political activist. ", password: "password", image_url: "https://www.smartheadshots.com/actor-headshots/headshots/headshots-5225.jpg"},
+  {username: 'twittle12', email: 'twittle12@email.com', bio: "Australian actor, singer, and producer", password: "password", image_url: "https://lh3.googleusercontent.com/proxy/QviL5YhnlFw1P-ohVQXRGxfLgpEomiZrvv47SMPOOapERPbcllLih56CXdbNO-m7TxjnEJcUWEwWjNAhHKWs71hZX39W1ltZMZOPIH-NOe2brFtKo_tqN2HD0eFx2ICk"},
   {username: 'slackman', email: 'slackmantea79@email.com', bio: "American actress, filmmaker, and humanitarian.", password: "password", image_url: "https://highlinewest.com/wp/wp-content/uploads/2013/07/Lien-Yeung-Vancouver-headshot-photographer-cbc-commercial-photography-2-1.jpg"},
   {username: 'babychewy', email: 'chewinggum@email.com', bio: "American actress, filmmaker, and humanitarian.", password: "password", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTuluQEDuS9enMQGdkSkUqKysTz8aY3uAimYdFuGfLILtql4AkA"},
+  {username: 'timewatch365', email: 'timewatch365@email.com', bio: "American actress, filmmaker, and humanitarian.", password: "password", image_url: "https://s23527.pcdn.co/wp-content/uploads/2019/04/190308-Albert-Leslie-213-Roberto-Vivancos-Studio.jpg.optimal.jpg"},
 
 ])
 
