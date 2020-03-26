8fd0896c66129c7e70fcc2046df6c5219ba315cb
christine <credhead89@gmail.com>
2020-03-26 19:19:45 -0400

    Test

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,16 +19,21 @@
 # user.save
 
 # User.create!([
-#   {username: 'bbmax', email: 'bbmax@gmail.com', bio: "I am a Mexican and American film actress and producer. I began my career in Mexico starring in the telenovela Tammie and starred in the film El Callejón de los Milagros. I consider my self to be a well seasoned film actress, always looking for new projects.", password: "password", image_url: "https://i.pinimg.com/originals/51/79/7f/51797f1fe0b98932291b87527c115470.jpg"},
-#   {username: 'cad879', email: 'cad879@gmail.com', bio: "Actor from Ohio", password: "password", image_url: "https://i.pinimg.com/originals/29/80/94/298094c2d1dbf6267d5e61d741b902c2.jpg"},
-#   {username: 'gobbsam13', email: 'gobb13@email.com', bio: "Actor from LA", password: "password", image_url: "https://i.pinimg.com/474x/22/99/fb/2299fb7caf07e0df716fb7c0db322f43.jpg"},
-#   {username: 'swaddle56', email: 'swaddle56@email.com', bio: "American actor, writer, producer, comedian, and political activist. ", password: "password", image_url: "https://www.smartheadshots.com/actor-headshots/headshots/headshots-5225.jpg"},
-#   {username: 'twittle12', email: 'twittle12@email.com', bio: "While battling a trio of rampaging giants, the legendary half-demon Hellboy encounters Nimue the Blood Queen, an ancient resurrected sorceress who's out to avenge a past betrayal. Suddenly caught in a clash between the supernatural and the human, Hellboy soon becomes hell-bent on stopping Nimue with", password: "password", image_url: "https://lh3.googleusercontent.com/proxy/QviL5YhnlFw1P-ohVQXRGxfLgpEomiZrvv47SMPOOapERPbcllLih56CXdbNO-m7TxjnEJcUWEwWjNAhHKWs71hZX39W1ltZMZOPIH-NOe2brFtKo_tqN2HD0eFx2ICk"},
-#   {username: 'slackman', email: 'slackmantea79@email.com', bio: "American actress, filmmaker, and humanitarian.", password: "password", image_url: "https://highlinewest.com/wp/wp-content/uploads/2013/07/Lien-Yeung-Vancouver-headshot-photographer-cbc-commercial-photography-2-1.jpg"},
-#   {username: 'babychewy', email: 'chewinggum@email.com', bio: "An actor from Hawaii. One of the Rebel Alliance's greatest leaders, fearless on the battlefield and dedicated to ending the tyranny of the Empire. Daughter of Padmé Amidala and Anakin Skywalker, sister of Luke Skywalker, and with a soft spot for scoundrels, Leia ranks among the galaxy's great heroes.", password: "password", image_url: "https://images.squarespace-cdn.com/content/v1/54d942d5e4b0c4d5e7b8e4f0/1556560089603-ZUSLF1F8R69GGZZQNR2W/ke17ZwdGBToddI8pDm48kJ58XeMfiK7IQEXoMp_JnRZZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVEJBeI0DUjcvx4IkQNhOjnx31NrqoOC08IXFHVKI4tSfn4UqblQw_7Z37AUOnq6DEc/Corporate-headshots.jpg"},
-#   {username: 'timewatch365', email: 'timewatch365@email.com', bio: "American actress, filmmaker, and humanitarian.", password: "password", image_url: "https://s23527.pcdn.co/wp-content/uploads/2019/04/190308-Albert-Leslie-213-Roberto-Vivancos-Studio.jpg.optimal.jpg"},
-#   {username: 'cattiebigtime', email: 'cattie85@gmail.com', bio: "is an American actor, filmmaker, author and musician. He is best known for portraying Ted Mosby on the popular Emmy Award-winning CBS sitcom How I Met Your Mother. He made his writing and directorial debut with the 2010 comedy drama film", password: "password", image_url: "https://images.squarespace-cdn.com/content/v1/58677112e58c62e1bf0a8ca5/1540499086172-TF60L9RHXYU8QADUI90D/ke17ZwdGBToddI8pDm48kCiC6x0kNpxliGpzQTkOLNUUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8PaoYXhp6HxIwZIk7-Mi3Tsic-L2IOPH3Dwrhl-Ne3Z2ppk8fIk4OUdzzAJ58dNz9ksAnbOUWC6fgQEHg0Bz14hkOpdljO7Z-5qh0zg85Jnj/Headshots.jpg?format=500w"},
-#   {username: 'credhead5', email: ' credhead89@gmail.com', bio: "is an American actor, filmmaker, author and musician. He is best known for portraying Ted Mosby on the popular Emmy Award-winning CBS sitcom How I Met Your Mother. He made his writing and directorial debut with the 2010 comedy drama film", password: "password", image_url: "https://www.smartheadshots.com/actor-headshots/headshots/actor-headshot-9878.jpg"},
+#   {username: 'BBMAX', email: 'bbmax@gmail.com', bio: "I am a Mexican and American film actress and producer. I began my career in Mexico starring in the telenovela Tammie and starred in the film El Callejón de los Milagros. I consider my self to be a well seasoned film actress, always looking for new projects.", password: "password", image_url: "https://i.pinimg.com/originals/51/79/7f/51797f1fe0b98932291b87527c115470.jpg"},
+#   {username: 'Cad879', email: 'cad879@gmail.com', bio: "Actor from Ohio, is an American actor, producer, businessman, model, rapper, singer and songwriter. He is also known by his former stage. Living in New York, currently on broadway in a play called The Last Man Standing.", password: "password", image_url: "https://www.jeremyfolmer.com/wp-content/uploads/2018/08/justinkirkland.jpg"},
+#   {username: 'Gobbsam13', email: 'gobb13@email.com', bio: "Im an American actor, director, producer and activist. Currently living in New York. Ive been in the films The Cabin in the Woods, The Butler and Band Aid and now i am in Law & Order. My dream job!!!", password: "password", image_url: "https://i.pinimg.com/474x/22/99/fb/2299fb7caf07e0df716fb7c0db322f43.jpg"},
+#   {username: 'Swaddle56', email: 'swaddle56@email.com', bio: "American actor, writer, producer, comedian, and political activist. I worked on The Water Horse The Almighty Johnsons and American Playboy: The Hugh Hefner Story. I love film and always looking for new projects to become a well rounded human being.", password: "password", image_url: "https://www.smartheadshots.com/actor-headshots/headshots/headshots-5225.jpg"},
+#   {username: 'RealDisneyPrincess', email: 'imadisneyprincess@email.com', bio: "I am a Rebel Alliance's leader, fearless on the battlefield and dedicated to ending the tyranny of the Empire. Daughter of Padmé Amidala and Anakin Skywalker, sister of Luke Skywalker, and with a soft spot for scoundrels, Leia ranks among the galaxy's great heroes.", password: "password", image_url: "https://i.pinimg.com/474x/af/a4/21/afa421c363d57ee3c01d936fc1aa5ba6--star-wars-princess-leia-leia-star-wars.jpg"},
+#   {username: 'Slackman', email: 'slackmantea79@email.com', bio: "I'm an actress, known for her roles as Kelly in Black Mirror, Dido Elizabeth Belle in Belle, Noni Jean in Beyond the Lights, Plumette in the live-action adaptation of Disney's Beauty and the Beast, and Hannah Shoenfeld in The Morning Show.", password: "password", image_url: "https://i.pinimg.com/originals/16/f0/81/16f081bef197bbc55b2a76ba7b8bb6c4.jpg"},
+#   {username: 'babychewy', email: 'chewinggum@email.com', bio: "A legendary Wookiee warrior and Han Solo's co-pilot aboard the Millennium Falcon, Chewbacca was part of a core group of Rebels who restored freedom to the galaxy. Known for his short temper and accuracy with a bowcaster, Chewie also has a big heart and is unwavering in his loyalty to his friends.", password: "password", image_url: "https://i.pinimg.com/474x/40/f9/71/40f971e340ee1a066904d08e9352484c.jpg"},
+#   {username: 'Timewatch365', email: 'timewatch365@email.com', bio: "Is an English actor. He is known for his portrayal of Frankie in the 2017 film Beach Rats, for which he was nominated for Independent Spirit Award for Best Male Lead, London Film Critics Circle Award for Young British/Irish Performer of the Year.", password: "password", image_url: "https://mad-photography.co.uk/wp-content/uploads/2016/05/Joe-McGann-Actors-headshot-London-MAD-Photography-Actors-portrait-Spotlight-Casting-head-shots-408x600.jpg"},
+#   {username: 'Cattiebigtime', email: 'cattie85@gmail.com', bio: "I'm an actor, filmmaker, author and musician. He is best known for portraying Ted Mosby on the popular Emmy Award-winning CBS sitcom How I Met Your Mother. He made his writing and directorial debut with the 2010 comedy drama film", password: "password", image_url: "https://headshotlove.com/wp-content/uploads/2018/07/DEAV2104-e1530914310860.jpg"},
+#   {username: 'Credhead5', email: 'credhead89@gmail.com', bio: "is an American actor, filmmaker, author and musician. He is best known for portraying Ted Mosby on the popular Emmy Award-winning CBS sitcom How I Met Your Mother. He made his writing and directorial debut with the 2010 comedy drama film", password: "password", image_url: "https://www.smartheadshots.com/actor-headshots/headshots/actor-headshot-9878.jpg"},
+#   {username: 'Telescopeman', email: 'telescopeman@gmail.com', bio: "I work both in film and on stage. I began my career in Russia starring in my own webseries called Chicken Riot and starred in the film Telegrapgh. I consider my self to be a fun and reliable. I am always looking for new projects and I love collaberating.", password: "password", image_url: "https://images.squarespace-cdn.com/content/v1/5261fe2de4b0790ad9aaad5f/1583875317618-6J8OILTZ3RISN7RILIKK/ke17ZwdGBToddI8pDm48kNu93_l1Rc0JoXikXAEKHf17gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmDJyaVitQ06bkWUY0OMxkmN-bdz7wg8la12Me-ub45vBE5029s6uMXtkNCzVgxK8m/actor-headshots-nyc-new-york-city_Megan-Channelle-399-Edit-Hi-Res.jpg?format=500w"},
+#   {username: 'WhatAboutMe', email: 'wam@email.com', bio: "Im a television personality and beauty pageant titleholder. I received national recognition after appearing as a contestant on season 1 of The Bachelor. I was eliminated during week 2. I was crowned Miss Texas USA in 2019", password: "password", image_url: "https://images.squarespace-cdn.com/content/v1/5261fe2de4b0790ad9aaad5f/1583875317618-6J8OILTZ3RISN7RILIKK/ke17ZwdGBToddI8pDm48kNu93_l1Rc0JoXikXAEKHf17gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmDJyaVitQ06bkWUY0OMxkmN-bdz7wg8la12Me-ub45vBE5029s6uMXtkNCzVgxK8m/actor-headshots-nyc-new-york-city_Megan-Channelle-399-Edit-Hi-Res.jpg?format=500w"},
+#   {username: 'Ramona_Dixion', email: 'rdixion@email.com', bio: "While battling a trio of rampaging giants, the legendary half-demon Hellboy encounters Nimue the Blood Queen, an ancient resurrected sorceress who's out to avenge a past betrayal. Suddenly caught in a clash between the supernatural and the human, Hellboy soon becomes hell-bent on stopping Nimue with", password: "password", image_url: "https://img1.looper.com/img/gallery/the-untold-truth-of-hellboy/intro-1552678673.jpg"},
+#   {username: 'Loverboy20', email: 'gobb13@email.com', bio: "I am a singer, songwriter and actor. I began acting in theater at the age of seven, I live in Detroit and am visiting New York/working for the next month. I am looking to make further connections in the industry as well as learn new techniques to elevate my craft to the next level.", password: "password", image_url: "https://www.smartheadshots.com/actor-headshots/headshots/moody-headshot-19330.jpg"},
+#    {username: 'Terminate1994', email: 'terminatethestage@email.com', bio: "I'm an English actor, film producer, and musician living in New York. Ive studied classical works for more than 20 years. I consider my self an expert in all Shakespeare.", password: "password", image_url: "https://i2-prod.gloucestershirelive.co.uk/sport/football/football-news/article252905.ece/ALTERNATES/s615/Terminator.jpg"},
 
 # ])
 
@@ -38,40 +43,34 @@
 
 
 
-# Meeting.create!([
-#   {seeker_id: 1, fulfiller_id: 6, address: "520 8th Ave New York, NY 10018", start_time: DateTime.new, end_time: DateTime.new + 1.hour},
-#   {seeker_id: 2, fulfiller_id: 7, address: "244 W 54th St, New York, NY 10019", start_time: DateTime.new, end_time: DateTime.new + 1.hour},
-#   {seeker_id: 3, fulfiller_id: 1, address: "520 8th Ave New York, NY 10018", start_time: DateTime.new, end_time: DateTime.new + 1.hour},
-#   {seeker_id: 4, fulfiller_id: 2, address: "2067 Broadway, New York, NY 10023", start_time: DateTime.new, end_time: DateTime.new + 1.hour},
-#   {seeker_id: 5, fulfiller_id: 3, address: "134 W 29th New York, NY 10001", start_time: DateTime.new, end_time: DateTime.new + 1.hour},
-#   {seeker_id: 6, fulfiller_id: 4, address: "2067 Broadway, New York, NY 10023", start_time: DateTime.new, end_time: DateTime.new + 1.hour},
-#   {seeker_id: 7, fulfiller_id: 5, address: "2067 Broadway, New York, NY 10023", start_time: DateTime.new, end_time: DateTime.new + 1.hour},
-#   {seeker_id: 8, fulfiller_id: 4, address: "2067 Broadway, New York, NY 10023", start_time: DateTime.new, end_time: DateTime.new + 1.hour},
+Meeting.create!([
+  {seeker_id: 1, fulfiller_id: 6, address: "520 8th Ave New York, NY 10018", start_time: DateTime.new, end_time: DateTime.new + 1.hour},
+  {seeker_id: 2, fulfiller_id: 7, address: "244 W 54th St, New York, NY 10019", start_time: DateTime.new, end_time: DateTime.new + 1.hour},
+  {seeker_id: 3, fulfiller_id: 1, address: "520 8th Ave New York, NY 10018", start_time: DateTime.new, end_time: DateTime.new + 1.hour},
+  {seeker_id: 4, fulfiller_id: 2, address: "2067 Broadway, New York, NY 10023", start_time: DateTime.new, end_time: DateTime.new + 1.hour},
+  {seeker_id: 5, fulfiller_id: 3, address: "134 W 29th New York, NY 10001", start_time: DateTime.new, end_time: DateTime.new + 1.hour},
+  {seeker_id: 6, fulfiller_id: 4, address: "2067 Broadway, New York, NY 10023", start_time: DateTime.new, end_time: DateTime.new + 1.hour},
+  {seeker_id: 7, fulfiller_id: 5, address: "2067 Broadway, New York, NY 10023", start_time: DateTime.new, end_time: DateTime.new + 1.hour},
+  {seeker_id: 8, fulfiller_id: 4, address: "2067 Broadway, New York, NY 10023", start_time: DateTime.new, end_time: DateTime.new + 1.hour},
   
 
-# ])
-
-
-#  Review.create!([
-#   {evaluator_id: 9, actor_id: 1, text: "will use again, true life saver."},
-#   {evaluator_id: 8, actor_id: 2, text: "Great Job, hope to use you again"},
-#   {evaluator_id: 3, actor_id: 3, text: "will use again, so grateful for your amazingness"},
-#   {evaluator_id: 10, actor_id: 4, text: "This individual is the most unreliable human ever!!!"},
-#   {evaluator_id: 4, actor_id: 5, text: "You are a god sent, hope to use you again"},
-#   {evaluator_id: 5, actor_id: 6, text: "Such an amazing actor and an even better person. I cant wait to create art together."},
-#   {evaluator_id: 2, actor_id: 7, text: "So amazing, when I start filmingagin you will be the first to be cast."},
-#   {evaluator_id: 6, actor_id: 8, text: "Extremely rude and unreliable. We met up at Ripley Grier Studios near her house for convenience reasons. So that means i had to trek 1 hour away from my place. Of course she was late and basically had no regards for my time."},
-#   {evaluator_id: 7, actor_id: 9, text: "The best guy ever, just amazing!!! Thanks a lot for your help."},
-#   {evaluator_id: 5, actor_id: 10, text: "When you look up the word extraordinary, you see miss Credhead5's picture there. Thank you for being so amazing."},
- 
-# ])
-
-
-
-
-User.create!([
-  {username: 'telescopeman', email: 'telescopeman@gmail.com', bio: "I work both in film and on stage. I began my career in Russia starring in my own webseries called Chicken Riot and starred in the film Telegrapgh. I consider my self to be a fun and reliable. I am always looking for new projects and I love collaberating.", password: "password", image_url: "https://images.squarespace-cdn.com/content/v1/5261fe2de4b0790ad9aaad5f/1583875317618-6J8OILTZ3RISN7RILIKK/ke17ZwdGBToddI8pDm48kNu93_l1Rc0JoXikXAEKHf17gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmDJyaVitQ06bkWUY0OMxkmN-bdz7wg8la12Me-ub45vBE5029s6uMXtkNCzVgxK8m/actor-headshots-nyc-new-york-city_Megan-Channelle-399-Edit-Hi-Res.jpg?format=500w"},
-  {username: 'WhatAboutMe', email: 'wam@email.com', bio: "Im a television personality and beauty pageant titleholder. I received national recognition after appearing as a contestant on season 1 of The Bachelor. I was eliminated during week 2. I was crowned Miss Texas USA in 2019", password: "password", image_url: "https://images.squarespace-cdn.com/content/v1/5261fe2de4b0790ad9aaad5f/1583875317618-6J8OILTZ3RISN7RILIKK/ke17ZwdGBToddI8pDm48kNu93_l1Rc0JoXikXAEKHf17gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QHyNOqBUUEtDDsRWrJLTmDJyaVitQ06bkWUY0OMxkmN-bdz7wg8la12Me-ub45vBE5029s6uMXtkNCzVgxK8m/actor-headshots-nyc-new-york-city_Megan-Channelle-399-Edit-Hi-Res.jpg?format=500w"},
-  {username: 'Ramona_Dixion', email: 'rdixion@email.com', bio: "While battling a trio of rampaging giants, the legendary half-demon Hellboy encounters Nimue the Blood Queen, an ancient resurrected sorceress who's out to avenge a past betrayal. Suddenly caught in a clash between the supernatural and the human, Hellboy soon becomes hell-bent on stopping Nimue with", password: "password", image_url: "https://img1.looper.com/img/gallery/the-untold-truth-of-hellboy/intro-1552678673.jpg"},
-  
 ])
+
+
+ Review.create!([
+  {evaluator_id: 9, actor_id: 1, text: "will use again, true life saver."},
+  {evaluator_id: 8, actor_id: 2, text: "Great Job, hope to use you again"},
+  {evaluator_id: 3, actor_id: 3, text: "will use again, so grateful for your amazingness"},
+  {evaluator_id: 10, actor_id: 4, text: "This individual is the most unreliable human ever!!!"},
+  {evaluator_id: 4, actor_id: 5, text: "You are a god sent, hope to use you again"},
+  {evaluator_id: 1, actor_id: 6, text: "Such an amazing actor and an even better person. I cant wait to create art together."},
+  {evaluator_id: 2, actor_id: 7, text: "So amazing, when I start filmingagin you will be the first to be cast."},
+  {evaluator_id: 6, actor_id: 5, text: "Extremely rude and unreliable. We met up at Ripley Grier Studios near her house for convenience reasons. So that means i had to trek 1 hour away from my place. Of course she was late and basically had no regards for my time."},
+  {evaluator_id: 7, actor_id: 9, text: "The best guy ever, just amazing!!! Thanks a lot for your help."},
+  {evaluator_id: 5, actor_id: 10, text: "When you look up the word extraordinary, you see miss Credhead5's picture there. Thank you for being so amazing."},
+ 
+])
+
+
+
+
